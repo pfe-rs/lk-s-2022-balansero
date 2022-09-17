03c4f4b5005c4c9ddf24a8ed20632ea85de54275
apantelic <apantelic583@gmail.com>
2022-09-18 00:22:37 +0200

    Update noviKod.ino

--- noviKod/noviKod.ino
+++ noviKod/noviKod.ino
@@ -2,8 +2,8 @@
 #include <Encoder.h>
 
 #define UGAO_PO_IMPULSU 360.0 / 8000.0
-#define directionPin 8
-#define ledPin 9
+//#define directionPin 8
+//#define ledPin 9
 
 const int MPU = 0x68; // MPU6050 I2C address
 float AccX, AccY, AccZ;
@@ -18,14 +18,20 @@ int c = 0;
 
 unsigned long dt = 5000;
 unsigned long previous_time = 0;
+ 
 
-volatile int counter; 
+//int motor1pin1 = 4;
+//int motor1pin2 = 5;
 
-int motor1pin1 = 4;
-int motor1pin2 = 5;
+// Motor B connections
+int enB = 10;
+int i_p3 = 4;
+int i_p4 = 5;
 
-
-float enkoder() {
+volatile int counter;
+//volatile int totalCounter;
+void enkoder() {
+  //totalCounter++;
   //static Encoder enkoder(2, 3);
   if(digitalRead(2)) counter++;
   else counter--;
@@ -37,23 +43,29 @@ float readEncoderData(){
   }
 
 void driveMotor(int speed){
-  if(speed>0){digitalWrite(directionPin,LOW);analogWrite(ledPin,abs(speed));};
-  if(speed<=0){digitalWrite(directionPin,HIGH);analogWrite(ledPin,255-abs(speed));};
+  //if(speed>0){digitalWrite(directionPin,LOW);analogWrite(ledPin,abs(speed));};
+  //if(speed<=0){digitalWrite(directionPin,HIGH);analogWrite(ledPin,255-abs(speed));};
 }
 
 
 void setup() {
   Serial.begin(115200);
-  
-  MPU6050_initialization();
-  delay(20);
 
-  pinMode(motor1pin1, INPUT_PULLUP);
-  pinMode(motor1pin2, INPUT_PULLUP);
-  pinMode(directionPin,OUTPUT);
+  // initialize imu
+  MPU6050_initialization();
+
+  // initialize encoder
   attachInterrupt(digitalPinToInterrupt(3), enkoder, RISING);
 
+  // initialize motor driver
+  driver_initialize();
+
   calculate_IMU_error();
+
+  gyroAngleX = 0;
+  gyroAngleY = 0;
+  gyroAngleZ = 0;
+  currentTime = millis();
 }
 
 
@@ -63,6 +75,13 @@ void MPU6050_initialization() {
   Wire.write(0x6B);                  // Talk to the register 6B
   Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
   Wire.endTransmission(true);        //end the transmission
+  delay(20);
+}
+
+void driver_initialize() {
+  pinMode(enB, OUTPUT);
+  pinMode(i_p3, OUTPUT);
+  pinMode(i_p4, OUTPUT);
 }
 
 float read_accel_data() {
@@ -76,33 +95,36 @@ float read_accel_data() {
   float AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
   
   // Calculating Roll and Pitch from the accelerometer data
-  float accAngleX = atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) - AccErrorX; // AccErrorX ~(-1.11) See the calculate_IMU_error()custom function for more details
-  float accAngleY = atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) - AccErrorY; // AccErrorY ~(-3.37)
+  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY; // AccErrorY ~(-1.58)
 
-  //return accAngleX;
-  return accAngleY;
+  return accAngleX;
+  //return accAngleY;
 }
 
-float read_gyro_data() {
+float read_gyro_data(float elapsedTime) {
   // === Read gyroscope data === //
   Wire.beginTransmission(MPU);
   Wire.write(0x43); // Gyro data first register address 0x43
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
 
-  const float gyro_to_radian = PI/(180.0*131.0); // 131.0 is from datasheet
-  float GyroX = (Wire.read() << 8 | Wire.read()) * gyro_to_radian; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  float GyroY = (Wire.read() << 8 | Wire.read()) * gyro_to_radian;
-  float GyroZ = (Wire.read() << 8 | Wire.read()) * gyro_to_radian;
-  
+  float GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
+  float GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  float GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
   // Correct the outputs with the calculated error values
-  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-2.68)
-  GyroY = GyroY - GyroErrorY; // GyroErrorY ~(-1.92)
-  GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-1.18)
+  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-0.56)
+  GyroY = GyroY - GyroErrorY; // GyroErrorY ~(2)
+  GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-0.8)
+  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
+  gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
+  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
+  yaw =  yaw + GyroZ * elapsedTime;
+  // Complementary filter - combine acceleromter and gyro angle values
+  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
+  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
 
-  //return GyroX;
-  return GyroY;
-  //return GyroZ;
+  return gyroAngleX;
 }
 float degree_to_radian(float degree) {return (degree * PI)/180.0;}
 float radian_to_degree(float radian) {return (radian * 180.0)/PI;}
@@ -161,30 +183,67 @@ void calculate_IMU_error() {
 }
 
 void loop() {
+  previousTime = currentTime;        // Previous time is stored before the actual time read
+  currentTime = millis();            // Current time actual time read
+  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
+  
   float accAngleY = read_accel_data();
     
-  float GyroY = read_gyro_data();
+  float GyroY = read_gyro_data(elapsedTime);
 
   float tau = 0.2;
   float alpha = tau/(tau + 0.005);
   pitch = (1 - alpha)*(pitch + (GyroY * dt)/1000000.0) + alpha * accAngleY;
   
-  // Print the values on the serial monitor
-  Serial.print(pitch);
-  Serial.println();
+  // Print the values of IMU on the serial monitor
+//  Serial.print(accAngleY);
+//  Serial.print(" ");
+//  Serial.print(GyroY);
+//  Serial.print(" ");
+//  Serial.print(pitch);
+//  Serial.print(" ");
+//  Serial.println();
+
+  float enkoder_ugao = readEncoderData();
+
+//  Serial.print(enkoder_ugao);
+//  Serial.print(" ");
+//  Serial.print(totalCounter);
+//  Serial.println();
+
+  // kontroler
+
+    analogWrite(enB, 100);
+  digitalWrite(i_p3, LOW);
+  digitalWrite(i_p4, HIGH);
+    delay(3000);
+  // Accelerate
+  /*for (int i = 0; i < 255; i++)
+  {
+    analogWrite(enB, 255);
+    Serial.println(i);
+    delay(30);
+  }
+  // Decelerate
+  for (int j = 255; j >= 0; --j)
+  {
+    analogWrite(enB, j);
+    Serial.println(j);
+    delay(30);
+  }*/
+  
+    analogWrite(enB, 100);
+  digitalWrite(i_p3, HIGH);
+  digitalWrite(i_p4, LOW);
+    delay(3000);
+  
+
+//
+//  motor_input = 0;
+//  driveMotor(motor_input);
 
   // Time control
   while(micros() < previous_time + dt) {}
   previous_time = micros();
-
-  float enkoder_ugao = readEncoderData();
-
-  Serial.println(enkoder_ugao);
-
-  // kontroler
-  if (enkoder_ugao > 0) counter ++;
-  else counter --;
-
-  driveMotor(counter);
-  delay(1000);
+  //delay(100);
 }
